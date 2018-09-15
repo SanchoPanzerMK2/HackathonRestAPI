2a9d8dec08bea9db1624427e65a8ca56d800861b
Armando Mercado <8243453+SanchoPanzerMK2@users.noreply.github.com>
2018-09-15 03:54:21 -0400

    Initial Upload

---
+++ app.js
@@ -0,0 +1,5 @@
+var settings = require('./settings.json');
+var querystring = require('querystring');
+url = require('url');
+var https = require('https');
+function createAccount(settings,response)
